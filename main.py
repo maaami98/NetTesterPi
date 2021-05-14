f657eb1d8b425afb528ba006985184520d076715
i60Mhz <i60mhz@localhost.localdomain>
2021-05-14 04:27:12 +0300

    info rebuilding

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from sys import platform
 from kivy.app import App
+from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-
+from kivy.graphics import Color
+from sys import exit
 from kivy.core.window import Window
+from kivy.uix.dropdown import DropDown
+
 Window.size = (480, 320)
 
 import subprocess
 import threading
 import platform
-
+import selectors
+import netifaces 
 class MainApp(App):
     def build(self):
         main_layout = BoxLayout(orientation="horizontal")
@@ -18,6 +23,7 @@ class MainApp(App):
         side_bar.size_hint = (0.2, 1.0)
         screen = BoxLayout(orientation="vertical")
         self.result_screen=  BoxLayout(orientation="vertical")
+        self.info_screen =  BoxLayout(orientation="vertical")
         self.main_layout=main_layout
         self.screen=screen
         self.side_bar=side_bar
@@ -26,11 +32,40 @@ class MainApp(App):
         self.last_button = None
         self.process=None
         
+        """ Info Page """
+        netif = DropDown()
+        for nic in netifaces.interfaces():
+            btn =Button(text=nic,size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: netif.select(btn.text))
+            btn.bind(on_press=self.refresh_info)
+            netif.add_widget(btn)
+        mainbutton = Button(text='Interfaces', halign="left",size_hint=(0.25,None))
+        mainbutton.bind(on_release=netif.open)
+        netif.bind(on_select=lambda instance, x: setattr(mainbutton,'text',x))
+        mainbutton.pos=(0,0)
+        self.ip_label=Label(text='IP:',markup=True)
+        self.net_label=Label(text='NET:',markup=True)
+        self.gw_label=Label(text='GW:',markup=True)
+        
+        default_if=netifaces.gateways()['default'][netifaces.AF_INET][1]
+        
+        try:
+            self.ip_label.text='[b]IP: '+list(netifaces.ifaddresses(default_if).values())[1][0]['addr']+'[/b]'
+            self.net_label.text='[b]NET: ' +list(netifaces.ifaddresses(default_if).values())[1][0]['netmask']+'[/b]'
+            self.gw_label.text='[b]GW: '+netifaces.gateways()['default'][netifaces.AF_INET][0]+'[/b]'
+        except:
+            self.ip_label.text='IP:'
+            self.net_label.text='NET:'
+            self.gw_label.text='GW:'
+        
+        
+        self.info_screen.add_widget(mainbutton)
+        self.info_screen.add_widget(self.ip_label)
+        self.info_screen.add_widget(self.net_label)
+        self.info_screen.add_widget(self.gw_label)
         self.target = TextInput(
             multiline=False, readonly=False, halign="right", hint_text="192.168.1.1"
         )
-        
-
         self.result = TextInput(
             multiline=True, readonly=True, halign="left", 
         )
@@ -84,7 +119,7 @@ class MainApp(App):
                     text="QUIT",
                     pos_hint={"center_x": 0.5, "center_y": 0.5},
                 ) 
-        button.bind(on_press=quit)
+        button.bind(on_press=exit)
         side_bar.add_widget(button)
         self.reset_color()
         side_bar.children[3].background_color =[1, 0, 0, 1] 
@@ -121,8 +156,22 @@ class MainApp(App):
         instance.background_color =[1, 0, 0, 1] 
         self.main_layout.remove_widget(self.result_screen)
         self.main_layout.remove_widget(self.screen)
-        self.main_layout.add_widget(self.screen)
-    
+        self.main_layout.remove_widget(self.info_screen)
+        if self.choise == 1:
+            self.main_layout.add_widget(self.info_screen)
+        else:
+            self.main_layout.add_widget(self.screen)
+    def refresh_info(self,instance):
+        default_if=instance.text
+        try:
+            self.ip_label.text='[b]IP: '+list(netifaces.ifaddresses(default_if).values())[1][0]['addr']+'[/b]'
+            self.net_label.text='[b]NET: ' +list(netifaces.ifaddresses(default_if).values())[1][0]['netmask']+'[/b]'
+            self.gw_label.text='[b]GW: '+netifaces.gateways()['default'][netifaces.AF_INET][0]+'[/b]'
+        except:
+            self.ip_label.text='IP:'
+            self.net_label.text='NET:'
+            self.gw_label.text='GW:'
+        
     def on_button_press(self, instance):
         current = self.target.text
         button_text = instance.text
@@ -144,9 +193,22 @@ class MainApp(App):
             new_text = current + button_text
             self.target.text = new_text
     def query(self,command):
-        self.process = subprocess.Popen(command,stdout=subprocess.PIPE)
+        self.process = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        sel = selectors.DefaultSelector()
+        sel.register(self.process.stdout,selectors.EVENT_READ)
+        sel.register(self.process.stderr,selectors.EVENT_READ)
+        while True:
+            for key, _ in sel.select():
+                data = key.fileobj.read1().decode()
+                if not data:
+                    break
+                self.result.text+=data
+                """
         for line in self.process.stdout:
             self.result.text+=line.decode()
+        for line in self.process.stderr:
+            self.result.text+=line.decode()
+            """
     def on_target(self, instance):
         text = self.target.text
         if text:
@@ -156,16 +218,16 @@ class MainApp(App):
                 if platform.system() == "Windows":
                     x = threading.Thread(target=self.query, args=('ping '+self.target.text+" -t",))
                 else:
-                    x = threading.Thread(target=self.query, args=('ping '+self.target.text,))
+                    x = threading.Thread(target=self.query, args=(['ping',self.target.text],))
                 x.start()
             if self.choise ==3:
                 if platform.system() == "Windows":
                     x = threading.Thread(target=self.query, args=('tracert '+self.target.text,))
                 else:
-                    x = threading.Thread(target=self.query, args=('traceroute '+self.target.text,))
+                    x = threading.Thread(target=self.query, args=(['traceroute',self.target.text],))
                 x.start()
             if self.choise ==4:
-                x = threading.Thread(target=self.query, args=('iperf3 -c '+self.target.text,))
+                x = threading.Thread(target=self.query, args=(['iperf3','-c ',self.target.text],))
                 x.start()
             
 if __name__ == '__main__':
